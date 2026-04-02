aea0c6866ade04acb020ee1d0494594bf6a0223b
Gabriel Desbordes <gabriel.desbordes@etu.ec-lyon.fr>
2026-04-02 15:53:51 +0200

    .

--- classes/matriceMbord.m
+++ classes/matriceMbord.m
@@ -1,13 +1,12 @@
-function [outputArg1,outputArg2] = matriceMbord(P,E)
-    nN= size(P,1)
-    nE= size(E,1)
-    S=[2 1,1 2]/6
-    M = sparse(NN, NN);
-    for n = 1:nE
-        I= Edges(n, :)
-        N= Nodes(I, :)
-        lambda= size(I,1)
-        M(I, I) = M(I, I) + lambda*S
+function M = matriceMbord(P,E)
+    nN= size(P,1);
+    nE= size(E,1);
+    S=[2 1;1 2]/6;
+    M = sparse(nN, nN);
+    for n = 1:nE;
+        I= E(n, :);
+        N= P(I, :);
+        lambda = norm(N(2,:) - N(1,:));
+        M(I,I) = M(I,I) + lambda * S;
     end 
-    return M
 end
